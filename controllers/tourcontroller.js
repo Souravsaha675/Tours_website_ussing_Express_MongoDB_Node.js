be67f200434996e37c3119f272e492075f5d38fa
sourav saha <souravsaha675@gmail.com>
2020-08-27 04:15:08 +0530

    Implementing Limiting & Pagination in Api

--- controllers/tourcontroller.js
+++ controllers/tourcontroller.js
@@ -29,6 +29,29 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //3)Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4)Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page dose not exist');
+      }
+    }
+
     //EXECUTE query
     const tours = await query;
 
