dec2d3d8210c8c0eb7cdad555d58ae9f650455d6
sourav saha <souravsaha675@gmail.com>
2020-08-27 03:44:04 +0530

    Filterring & sorting Query at API

--- controllers/tourcontroller.js
+++ controllers/tourcontroller.js
@@ -1,10 +1,38 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    //1A) FILTERING
+    const queryObj = { ...req.query };
 
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludeFields.forEach(el => delete queryObj[el]);
+
+    //1B) ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    //console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2)SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      //console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //EXECUTE query
+    const tours = await query;
+
+    //SEND response
     res.status(200).json({
       status: 'success',
       results: tours.length,
