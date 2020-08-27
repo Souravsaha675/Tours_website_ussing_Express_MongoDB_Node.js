afcd409a7e047c52f6a17b9243eae2d409e9828b
sourav saha <souravsaha675@gmail.com>
2020-08-27 23:42:11 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+/*const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);*/
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection established successfully'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`app listening on port ${port}...`);
+});
