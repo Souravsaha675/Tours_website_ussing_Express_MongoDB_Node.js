e13c3b7098e50c9eea87cf45c9ed6f0d9ef7fcea
sourav saha <souravsaha675@gmail.com>
2020-08-30 02:56:03 +0530

    using ndb ,Global error handling

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -11,14 +14,19 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log('Hello from middleware 👋');
-  next();
-});
-//2.ROUTE HANDLERS
-//3.ROUTES
+
+//2.ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 //4.SERVER
 module.exports = app;
