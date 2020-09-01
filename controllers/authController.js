4ea403306b2fc7bbc1d33f85786eca78d8b3f4e1
sourav saha <souravsaha675@gmail.com>
2020-09-02 00:22:23 +0530

    Protecting Routs

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,24 @@ exports.login = CatchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = CatchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  //console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError(
+        'You are not logged in! Please login first to get access.',
+        401
+      )
+    );
+  }
+  next();
+});
