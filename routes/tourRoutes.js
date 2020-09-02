c056dd039c12a66f7f3ac24612fe50a4416b2ea0
sourav saha <souravsaha675@gmail.com>
2020-09-03 01:01:29 +0530

    Password Reset Functionality Reset Token

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tourController = require('../controllers/tourcontroller');
-const authController = require('./../controllers/authController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,10 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.post('/forgotpassword', authController.forgotPassword);
+
+router.post('/resetpassword', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
