cecce5917a6265e3ac7e6455b882898939202491
sourav saha <souravsaha675@gmail.com>
2020-09-04 03:08:08 +0530

    HTTP headers,data Sanitization,Parameter Pollution

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,9 +14,14 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //1.MIDDLEWARS
+
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//LIMITIMG REQUEST FROM SAME IP
 
 const limiter = rateLimit({
   max: 100,
@@ -22,7 +31,35 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+//BODY-PARSER(READDING DATA FROM BODY INTO req.body)
+
+app.use(express.json({ limit: '10kb' }));
+
+//DATA SANITIZATION NOSQL QUERY INJECTION
+
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS
+
+app.use(xss());
+
+//PREVENT PARAMETER POLLUTION
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'maxGroupSize',
+      'price'
+    ]
+  })
+);
+
+//SERVING STATIC FILE
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
