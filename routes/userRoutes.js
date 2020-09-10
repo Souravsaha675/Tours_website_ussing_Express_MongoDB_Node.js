6e3808030a5e021b9dfc336b86d222eb825347bc
sourav saha <souravsaha675@gmail.com>
2020-09-10 23:42:26 +0530

    Adding Authentication & Authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,27 +6,17 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotpassword', authController.forgotPassword);
-
 router.patch('/resetpassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.patch('/deleteMe', userController.deleteMe);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-
-router.patch('/deleteMe', authController.protect, userController.deleteMe);
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
