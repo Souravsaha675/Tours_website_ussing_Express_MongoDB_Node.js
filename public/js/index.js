7f43af05c7cfbc2d9960b928632d3379be0b0785
sourav saha <souravsaha675@gmail.com>
2020-09-19 03:44:09 +0530

    Implemennting Log out

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable*/
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 const mapBox = document.getElementById('map');
 const loginFrom = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -18,4 +19,7 @@ if (loginFrom) {
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logout);
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/logout', authController.logOut);
 router.post('/forgotpassword', authController.forgotPassword);
 router.patch('/resetpassword/:token', authController.resetPassword);
 
