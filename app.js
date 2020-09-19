a61002e4c8628adb8a546dd79908afd61accd3db
sourav saha <souravsaha675@gmail.com>
2020-09-20 04:40:08 +0530

    Updating user password with our API

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
 
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+//app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use(cookieParser());
 
