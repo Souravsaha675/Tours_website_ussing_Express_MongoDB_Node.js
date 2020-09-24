d83e1488954d3ae5bdac298f28ad0e534c1f20b0
sourav saha <souravsaha675@gmail.com>
2020-09-24 22:49:06 +0530

    Creating New Bookings on Checkout success

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-
   //console.log(req.headers);
   //console.log(req.cookies);
   next();
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,13 +3,16 @@ const catchAsync = require('./../utils/catchAsync');
 //const AppError = require('./../utils/appError');
 //const APIFeatures = require('./../utils/apiFeatures');
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${req.params.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -29,4 +32,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
