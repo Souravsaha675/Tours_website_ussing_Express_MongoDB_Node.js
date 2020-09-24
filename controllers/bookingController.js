18379a03a325e33a54b8ea4f003c7d568f310dcc
sourav saha <souravsaha675@gmail.com>
2020-09-25 03:18:10 +0530

    Implementing Bookings API end points

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,6 +4,7 @@ const catchAsync = require('./../utils/catchAsync');
 //const APIFeatures = require('./../utils/apiFeatures');
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
@@ -43,3 +44,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
